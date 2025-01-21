e0d5c9983a7e0ec938f3278a6f69c851eb5d0654
OK <deniz.ok@epitech.eu>
2025-01-21 21:41:48 +0100

    Modification pour rendre le portfolio responsif

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        'noto-serif-jp': ['"Noto Serif JP"', 'serif'],
-      },
-      screens: {
-        'xs': '475px',
-      },
-    },
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [],
 };
